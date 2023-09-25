d4360298008f8f8d5ca4ccaaa0808cc86e5af7a4
Naveen-Ojha <naveen.ojha.rewa@gmail.com>
2023-09-25 07:34:49 +0530

    blogapp

--- users/component.js/DrawerNavigator.js
+++ users/component.js/DrawerNavigator.js
@@ -10,25 +10,26 @@ import CategoryScreen from "../pages/CategoryScreen";
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
-    const { categoryName } = useContext(CategoryContext)
-    const cateKeys = Object.keys(categoryName)
-    return (
-        <>
-            <Drawer.Navigator>
-                <Drawer.Screen name="Home" component={BottomTabNavigator} />
-                {
-                    cateKeys.map((cate) => {
-                        return (
-                            <>
-                                <Drawer.Screen key={categoryName[cate]['TermSlug']} name={categoryName[cate]['TermName']} initialParams={{ slug: categoryName[cate]['TermSlug'] }} component={CategoryScreen} />
-                            </>
-                        )
-                    })
-                }
-                <Drawer.Screen name="About" component={SettingStackNavigator} />
-            </Drawer.Navigator>
-        </>
-    );
-}
+  const { categoryName } = useContext(CategoryContext);
+  const cateKeys = Object.keys(categoryName);
+  return (
+    <>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Fashion Sootra" component={BottomTabNavigator} />
+        {cateKeys.map((cate) => {
+          return (
+            <Drawer.Screen
+              key={categoryName[cate]["TermSlug"]}
+              name={categoryName[cate]["TermName"]}
+              initialParams={{ slug: categoryName[cate]["TermSlug"] }}
+              component={CategoryScreen}
+            />
+          );
+        })}
+        <Drawer.Screen name="About" component={SettingStackNavigator} />
+      </Drawer.Navigator>
+    </>
+  );
+};
 
 export default DrawerNavigator;
--- users/pages/CategoryScreen.js
+++ users/pages/CategoryScreen.js
@@ -1,93 +1,114 @@
-import React, { useState, useEffect, useContext } from 'react'
-import { Text, View, ScrollView, SafeAreaView, StyleSheet, Image } from 'react-native'
-import moment from 'moment';
+import React ,{useState,useEffect, useContext} from 'react';
+import {  FlatList, Image, SafeAreaView,ScrollView, StyleSheet, Text, TouchableOpacity, View, ActivityIndicator } from 'react-native';
 import { apiEndPoint, domainPoint } from '../../environment';
+import moment from 'moment';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { CategoryContext } from '../context/CategoryContext';
 
+const CategoryScreen =({ navigation, route }) =>  {
 
-export default function CategoryScreen({ navigation, route }) {
-    const { categoryName } = useContext(CategoryContext)
-    const Separator = () => <View style={styles.separator} />;
-
-    const [response, setResponse] = useState([])
-    function getCategoryData() {
-        var requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-        };
-
-        fetch(`${apiEndPoint}category/${route.params.slug}`, requestOptions)
-            .then(response => response.json())
-            .then(result => setResponse(result)
-            )
-            .catch(error => console.log('error', error));
-    }
-
-    useEffect(() => {
-        getCategoryData()
-    }, [])
-
-    function validateText(str) {
-        var tarea = str;
-        if (tarea.indexOf("http://") == 0 || tarea.indexOf("https://") == 0) {
-            return true
-        } else {
-            return false
-        }
-    }
-
-    return (
-        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-            <SafeAreaView style={styles.container}>
-                {
-                    response.map((val) => {
-                        const { ID, PostTitle, CatId, PostThumbUrl, Date } = val
-                        const words = PostTitle.split(' ');
-                        const truncatedTitle = words.slice(0, 10).join(' ');
-                        const PostImages = validateText(PostThumbUrl)
-                        const getPostImages = PostImages ? PostThumbUrl : `${domainPoint}${PostThumbUrl}`
-                        return (
-                            <View key={ID}>
-                                <View style={[styles.card, styles.shadowProp]}>
-                                    <View style={styles.fixToText}>
-                                        <Image
-                                            style={styles.tinyLogo}
-                                            source={{
-                                                uri: getPostImages
-                                            }}
-                                        />
-                                        <View style={styles.titleBox}>
-                                            <Text onPress={() =>
-                                                navigation.navigate('Details', { user: ID })
-                                            }
-                                                style={styles.title}>{truncatedTitle}...</Text>
-                                            <View style={styles.fixToText}>
-                                                <Text>
-                                                    <MaterialCommunityIcons name="circle-medium" color="#000" size={20} style={{ alignItems: 'center' }} />
-                                                    {categoryName[CatId].TermName}</Text>
-                                                <Text>
-                                                    <MaterialCommunityIcons name="circle-medium" color="#000" size={20} style={{ alignItems: 'center' }} />
-                                                    {moment.utc(Date).local().startOf('seconds').fromNow()}</Text>
-                                            </View>
-                                        </View>
-                                    </View>
-                                </View>
-                                <Separator />
-                            </View>
-                        )
-                    })
-                }
-            </SafeAreaView>
-        </ScrollView>
-    )
+const { categoryName } = useContext(CategoryContext)
+const Separator = () => <View style={styles.separator} />;
+const [data,setData]=useState([])
+const [limit,setLimit]=useState(10)
+const apiurl=`${apiEndPoint}category/${route.params.slug}?_limit=${limit}`
+const fetchdata=()=>{
+  fetch(apiurl)
+  .then((response)=>response.json())
+  .then((jsondata)=>{setData(jsondata)})
+  .catch((err)=>console.log(err))
+}
+useEffect(()=>{
+fetchdata()
+},[])
+const render=({item})=>{
+    const words = item.PostTitle.split(' ');
+    const truncatedTitle = words.slice(0, 10).join(' ');
+    const PostImages = validateText(item.PostThumbUrl)
+    const getPostImages = PostImages ? item.PostThumbUrl : `${domainPoint}${item.PostThumbUrl}`
+  
+  return(
+     <View key={item.ID}>
+        <View style={[styles.card, styles.shadowProp]}>
+            <View style={styles.fixToText}>
+                <Image
+                    style={styles.tinyLogo}
+                    source={{
+                        uri: getPostImages
+                    }}
+                />
+                <View style={styles.titleBox}>
+                    <Text onPress={() =>
+                        navigation.navigate('Details', { user: item.ID })
+                    }
+                        style={styles.title}>{truncatedTitle}...</Text>
+                    <View style={styles.fixToText}>
+                        <Text>
+                            <MaterialCommunityIcons name="circle-medium" color="#000" size={20} style={{ alignItems: 'center' }} />
+                            {categoryName[item.CatId].TermName}</Text>
+                        <Text>
+                            <MaterialCommunityIcons name="circle-medium" color="#000" size={20} style={{ alignItems: 'center' }} />
+                            {moment.utc(item.Date).local().startOf('seconds').fromNow()}</Text>
+                    </View>
+                </View>
+            </View>
+        </View>
+        <Separator />
+    </View>
+  )
+}
+const loadmore=()=>{
+  console.log('end reached!!')
+  const Separator = () => <View>You Have Reached The End</View>;
+  setLimit(limit+5)
+  fetchdata()
+  console.log(apiurl)
 }
 
+const  validateText=(str)=> {
+    var tarea = str;
+    if (tarea.indexOf("http://") == 0 || tarea.indexOf("https://") == 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+const renderFooter = () =>{
+ return(
+       <View style={{justifyContent:"center",alignItems:'center'}}>
+         <ActivityIndicator size="large" color="#00ff00" />
+       </View>
+     )
+ }
+
+return (
+    
+    <SafeAreaView style={styles.container}>
+    
+
+ {/* <Text style={{fontSize:18,fontWeight:'bold'}}>React Native FlatList Load More On Scroll</Text> 
+  <Text style={{fontSize:16,fontWeight:'bold'}}>Programming with savio</Text> */}
+
+<FlatList
+data={data}
+renderItem={render}
+keyExtractor={(item,i)=>i.toString()}
+ListFooterComponent={renderFooter}
+onEndReached={()=>{loadmore()}}
+
+/>
+   </SafeAreaView>
+   
+  );
+};
 const styles = StyleSheet.create({
     container: {
         padding: 10,
         backgroundColor: '#fff',
         marginVertical: 6,
+        width:'100%',
+        flex:1
     },
     card: {
         backgroundColor: '#dcebfd',
@@ -141,3 +162,5 @@ const styles = StyleSheet.create({
         fontSize: 20
     }
 });
+
+export default CategoryScreen;
